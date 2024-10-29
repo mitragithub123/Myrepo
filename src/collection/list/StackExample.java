baa4f0cbc4e36ab82a46b54355221f68dbc46ec7
Mitra bhanu <andolasoft.user133@.com>
2024-10-29 12:13:57 +0530

    Stack example

---
+++ src/collection/list/StackExample.java
@@ -0,0 +1,64 @@
+package collection.list;
+
+import java.util.Iterator;
+import java.util.Stack;
+
+public class StackExample {
+	public static void main(String[] args) {
+		Stack<Integer> stack = new Stack<Integer>();
+
+		stack.push(10);
+		stack.push(20);
+		stack.push(30);
+		stack.push(40);
+		stack.push(50);
+		stack.push(60);
+		System.out.println(stack);
+
+		/* Peeking at the top element */
+		int topElement = stack.peek();
+		System.out.println("Top element: " + topElement);
+
+		/* Popping elements from the stack */
+		int poppedElement1 = stack.pop(); // removes and returns the top element
+		int poppedElement2 = stack.pop();
+		System.out.println("Popped elements: " + poppedElement1 + ", " + poppedElement2);
+
+		/* Checking if the stack is empty */
+		boolean isEmpty = stack.isEmpty();
+		System.out.println(isEmpty);
+
+		/* Searching for an element in the stack */
+		int searchedElement = stack.search(10);
+		if (searchedElement != -1) {
+			System.out.println("Element Found");
+		} else {
+			System.out.println("Element not Found");
+		}
+
+		/* Using a for loop with get(index) */
+		System.out.println("\nUsing an for loop:");
+		for (int i = 0; i < stack.size(); i++) {
+			System.out.println(stack.get(i));
+		}
+
+		/* Using an enhanced for loop */
+		System.out.println("\nUsing an enhanced for loop:");
+		System.out.println("\nUsing an iterator:");
+		for (Integer number : stack) {
+			System.out.println(number);
+		}
+
+		/* Using an iterator */
+		System.out.println("\nUsing an iterator:");
+		Iterator<Integer> iterator = stack.iterator();
+		while (iterator.hasNext()) {
+			System.out.println(iterator.next());
+		}
+		
+		/* Clearing the stack */
+        stack.clear();
+        System.out.println("Stack after clearing: " + stack);
+
+	}
+}
