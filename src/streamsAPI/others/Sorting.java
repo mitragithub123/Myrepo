70802e422f0d5b09b40a44c97453d6b48a46a0a9
Mitra bhanu <andolasoft.user133@.com>
2024-11-18 15:00:07 +0530

    Stream api reverse sorting

--- src/streamsAPI/others/Sorting.java
+++ src/streamsAPI/others/Sorting.java
@@ -1,6 +1,7 @@
 package streamsAPI.others;
 
 import java.util.Arrays;
+import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -10,10 +11,16 @@ public class Sorting {
 		List<Integer> numberList = Arrays.asList(1, 3, 66, 12, 34, 48);
 		List<Integer> sortedNumberList = numberList.stream().sorted().collect(Collectors.toList());
 		System.out.println(sortedNumberList);
+		List<Integer> sortedNumberListReverseOrder = numberList.stream().sorted(Comparator.reverseOrder())
+				.collect(Collectors.toList());
+		System.out.println(sortedNumberListReverseOrder);
 
 		List<String> vehiclesList = Arrays.asList("bus", "car", "bike", "train", "plane", "tank");
 		List<String> sortedvehiclesList = vehiclesList.stream().sorted().collect(Collectors.toList());
 		System.out.println(sortedvehiclesList);
+		List<String> sortedvehiclesListReverseOrder = vehiclesList.stream().sorted(Comparator.reverseOrder())
+				.collect(Collectors.toList());
+		System.out.println(sortedvehiclesListReverseOrder);
 
 	}
 
