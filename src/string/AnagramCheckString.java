1ae9b0a1cc121b1137b701b7d5374107b6a88ba1
Mitra bhanu <andolasoft.user133@.com>
2024-12-16 15:31:51 +0530

    Anagram Check 2 strings

---
+++ src/string/AnagramCheckString.java
@@ -0,0 +1,23 @@
+package string;
+
+import java.util.Arrays;
+
+// Check whether two strings are anagrams (contain the same characters in a different order).
+public class AnagramCheckString {
+
+	public static void main(String[] args) {
+		String str1 = "listen";
+		String str2 = "silent";
+
+		char[] charArray1 = str1.toCharArray();
+		char[] charArray2 = str2.toCharArray();
+
+		Arrays.sort(charArray1);
+		Arrays.sort(charArray2);
+
+		boolean isAnagram = Arrays.equals(charArray1, charArray2);
+		System.out.println("Strings are Anagrams: " + isAnagram);
+
+	}
+
+}
