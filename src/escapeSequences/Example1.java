7739479bff5520124159e01ad82e862a1544754a
Mitra bhanu <andolasoft.user133@.com>
2025-01-23 16:45:05 +0530

    Escape sequence application

---
+++ src/escapeSequences/Example1.java
@@ -0,0 +1,36 @@
+package escapeSequences;
+
+public class Example1 {
+
+	public static void main(String[] args) {
+		// Print "Hello" World (Printing Double Quotes in Strings)
+		System.out.println("\"Hello\" World");
+
+		// Printing Backslashes
+		// This is a backslash: \
+		System.out.println("This is a backslash: \\");
+
+		// Printing New Lines (\n)
+		System.out.println("Hello\nWorld");
+
+		// Printing Tabs (\t)
+		System.out.println("Name:\tJohn");
+		System.out.println("Age:\t25");
+
+		// Using Single Quotes (\')
+		// It's a great day to learn Java!
+		System.out.println("It\'s a great day to learn Java!");
+
+		// Using Unicode Characters
+		System.out.println("The symbol for pi is: \u03C0");
+		System.out.println("Keep smiling: \uD83D\uDE04");
+
+		// Printing a Carriage Return (\r)
+		System.out.println("Hello\rWorld");
+
+		// Printing a Backspace (\b)
+		System.out.println("ABC\bD");
+
+	}
+
+}
